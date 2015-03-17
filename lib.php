8d8869d36490023bfdce09f5e149abf2de46a4c8
James Hall <james_hall@live.ca>
2015-03-17 13:25:55 -0400

    Moved to github.com/BruteForceFencer/php-bff

---
+++ lib.php
@@ -0,0 +1,52 @@
+<?php
+
+class BFF {
+  private $type;
+  private $addr;
+  private $port;
+
+  public function __construct($type, $addr, $port = 9999) {
+    $this->type = $type;
+    $this->addr = $addr;
+    $this->port = $port;
+  }
+
+  public function getType() {
+    return $this->type;
+  }
+
+  public function getAddr() {
+    return $this->addr;
+  }
+
+  public function hit($direction, $value) {
+    $file = NULL;
+    if ($this->type === "unix") {
+      $file = pfsockopen("unix://" . $this->addr);
+    } else {
+      $file = pfsockopen("tcp://" . $this->addr, $this->port);
+    }
+
+    if ($file === FALSE) {
+      return FALSE;
+    }
+
+    $request = json_encode(array(
+      'Direction' => $direction,
+      'Value' => $value,
+    ));
+    fwrite($file, $request);
+
+    $valid = $this->readResponse($file);
+    return $valid;
+  }
+
+  private function readResponse($file) {
+    $data = fread($file, 1);
+    if ($data === "t") {
+      return TRUE;
+    } else {
+      return FALSE;
+    }
+  }
+}
